3ba28e34a5345dfa17830a4c0e8c45b062f61bf1
mahmooudzain <judgeman20@gmail.com>
2024-03-13 16:26:40 +0300

    fix project structure

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Footcomp() {
+  return <h1>fooot</h1>;
+}
+export default Footcomp;
--- travel/src/components/footer/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Footcomp() {
-  return <h1>fooot</h1>;
-}
-export default Footcomp;
